7df7669b88d0a6b966da5f0f547e84f7e8406ca6
El Topo <eltopo@live.com>
2018-05-07 10:35:34 -0700

    add restart script

---
+++ lxl-restart-zt.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# restart zerotier client
+
+# am I root?
+MYUID=$(id -u)
+if [ $MYUID -eq 0 ] 
+then 
+	SUDO=
+else
+	SUDO=sudo
+fi
+
+$SUDO killall -9 zerotier-one 
+$SUDO zerotier-one -d
+
